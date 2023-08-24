b07bb25345725e39308813760a9c2649723d980b
Oleksandr Druzhynin <aleks_str78@ukr.net>
2023-08-24 19:58:29 +0300

    add baseTypes

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,20 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum Weekdays {
+  Sunday,
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+}
+
+const isWeekend = (weekday: Weekdays): boolean => {
+  return weekday === Weekdays.Sunday || weekday === Weekdays.Saturday;
+};
+
+// console.log(isWeekend(Weekdays.Friday));
+// console.log(isWeekend(Weekdays.Sunday));
--- src/baseTypes/8.ts
+++ src/baseTypes/8.ts
@@ -3,6 +3,10 @@
   який може містити значення "male", "female". Створіть змінну myGender цього типу.
 */
 
-const myGender;
+type Gender = "male" | "female";
+let myGender: Gender;
+
+// let Gender: "male" | "female";
+// let myGender: typeof Gender;
 
 export {};
